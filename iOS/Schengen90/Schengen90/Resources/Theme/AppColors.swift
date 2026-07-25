355e39a7a5f2cafd800ac9b5a24e3b12ca3b6904
som-db2 <sskundu2025@gmail.com>
2026-07-25 15:58:53 +0530

    Complete theme design system

--- iOS/Schengen90/Schengen90/Resources/Theme/AppColors.swift
+++ iOS/Schengen90/Schengen90/Resources/Theme/AppColors.swift
@@ -5,3 +5,26 @@
 //  Created by Som Kundu on 25/07/26.
 //
 
+import SwiftUI
+
+enum AppColors {
+
+    // Brand Colors
+    static let primary = Color(red: 0.0, green: 122/255, blue: 1.0)
+
+    // Backgrounds
+    static let background = Color(.systemBackground)
+    static let secondaryBackground = Color(.secondarySystemBackground)
+
+    // Cards
+    static let card = Color(.systemGray6)
+
+    // Text
+    static let primaryText = Color.primary
+    static let secondaryText = Color.secondary
+
+    // Status
+    static let success = Color.green
+    static let warning = Color.orange
+    static let error = Color.red
+}
--- iOS/Schengen90/Schengen90/Resources/Theme/AppTypography.swift
+++ iOS/Schengen90/Schengen90/Resources/Theme/AppTypography.swift
@@ -5,3 +5,19 @@
 //  Created by Som Kundu on 25/07/26.
 //
 
+import SwiftUI
+
+enum AppTypography {
+
+    static let largeTitle = Font.largeTitle.weight(.bold)
+
+    static let title = Font.title.weight(.bold)
+
+    static let heading = Font.title2.weight(.semibold)
+
+    static let body = Font.body
+
+    static let bodyBold = Font.body.weight(.semibold)
+
+    static let caption = Font.caption
+}
