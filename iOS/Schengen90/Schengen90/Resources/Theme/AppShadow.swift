7d016ecb9c7dd72c4db48849e85dcea582263ff6
som-db2 <sskundu2025@gmail.com>
2026-07-25 16:09:26 +0530

    Create AppShadow.swift

---
+++ iOS/Schengen90/Schengen90/Resources/Theme/AppShadow.swift
@@ -0,0 +1,18 @@
+//
+//  AppShadow.swift
+//  Schengen90
+//
+//  Created by Som Kundu on 25/07/26.
+//
+
+import SwiftUI
+
+enum AppShadow {
+
+    static let radius: CGFloat = 4
+
+    static let x: CGFloat = 0
+
+    static let y: CGFloat = 2
+
+}
