c540ac8b1b6e7fa3732002802f7d9fca47a1be61
som-db2 <sskundu2025@gmail.com>
2026-07-26 04:14:09 +0530

    Update MainTabView.swift

--- iOS/Schengen90/Schengen90/Views/Main/MainTabView.swift
+++ iOS/Schengen90/Schengen90/Views/Main/MainTabView.swift
@@ -5,3 +5,78 @@
 //  Created by Som Kundu on 26/07/26.
 //
 
+import SwiftUI
+
+struct MainTabView: View {
+
+    var body: some View {
+
+        TabView {
+
+            NavigationStack {
+
+                HomeView()
+
+            }
+            .tabItem {
+
+                Label(
+                    "Home",
+                    systemImage: "house"
+                )
+
+            }
+
+            NavigationStack {
+
+                TripsView()
+
+            }
+            .tabItem {
+
+                Label(
+                    "Trips",
+                    systemImage: "airplane"
+                )
+
+            }
+
+            NavigationStack {
+
+                PlannerView()
+
+            }
+            .tabItem {
+
+                Label(
+                    "Planner",
+                    systemImage: "calendar"
+                )
+
+            }
+
+            NavigationStack {
+
+                SettingsView()
+
+            }
+            .tabItem {
+
+                Label(
+                    "Settings",
+                    systemImage: "gearshape"
+                )
+
+            }
+
+        }
+
+    }
+
+}
+
+#Preview {
+
+    MainTabView()
+
+}
