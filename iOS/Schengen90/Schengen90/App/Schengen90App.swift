c90dc9ee04452d928b6b0aa9de96dee14aaae561
som-db2 <sskundu2025@gmail.com>
2026-07-26 04:15:19 +0530

    Update Schengen90App.swift

--- iOS/Schengen90/Schengen90/App/Schengen90App.swift
+++ iOS/Schengen90/Schengen90/App/Schengen90App.swift
@@ -25,7 +25,7 @@ struct Schengen90App: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
         }
         .modelContainer(sharedModelContainer)
     }
