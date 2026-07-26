f370e987d5022cd2f27d0110e3995af385dc5767
som-db2 <sskundu2025@gmail.com>
2026-07-26 23:30:46 +0530

    Create StaySimulationResult.swift

---
+++ iOS/Schengen90/Schengen90/Models/StaySimulationResult.swift
@@ -0,0 +1,20 @@
+//
+//  StaySimulationResult.swift
+//  Schengen90
+//
+//  Created by Som Kundu on 26/07/26.
+//
+
+import Foundation
+
+struct StaySimulationResult {
+
+    let entryDate: Date
+
+    let latestLegalExit: Date?
+
+    let simulatedDays: Int
+
+    let isEntryAllowed: Bool
+
+}
