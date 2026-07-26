68dc7e34cee95e95172030f8a0c37ddfd4a0b599
som-db2 <sskundu2025@gmail.com>
2026-07-26 19:50:54 +0530

    Update Schengen90App.swift

--- iOS/Schengen90/Schengen90/App/Schengen90App.swift
+++ iOS/Schengen90/Schengen90/App/Schengen90App.swift
@@ -10,25 +10,13 @@ import SwiftData
 
 @main
 struct Schengen90App: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
+    
     var body: some Scene {
+        
         WindowGroup {
+            
             MainTabView()
         }
-        .modelContainer(
-            for: Trip.self
-        )
+        .modelContainer(for: Trip.self)
     }
 }
