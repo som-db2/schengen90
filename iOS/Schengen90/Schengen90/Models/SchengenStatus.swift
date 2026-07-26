609a4904334b1da1c898555fea1eaaf6298dc4ff
som-db2 <sskundu2025@gmail.com>
2026-07-26 23:05:21 +0530

    Create SchengenStatus.swift

---
+++ iOS/Schengen90/Schengen90/Models/SchengenStatus.swift
@@ -0,0 +1,20 @@
+//
+//  SchengenStatus.swift
+//  Schengen90
+//
+//  Created by Som Kundu on 26/07/26.
+//
+
+import Foundation
+
+struct SchengenStatus {
+
+    let referenceDate: Date
+
+    let occupiedDates: [Date]
+
+    let usedDays: Int
+
+    let remainingDays: Int
+
+}
