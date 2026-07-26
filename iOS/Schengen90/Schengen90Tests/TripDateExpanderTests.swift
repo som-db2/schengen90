c55d53e62033b47fe0f3f2848444eea9b8b45334
som-db2 <sskundu2025@gmail.com>
2026-07-26 22:14:04 +0530

    Update TripDateExpanderTests.swift

--- iOS/Schengen90/Schengen90Tests/TripDateExpanderTests.swift
+++ iOS/Schengen90/Schengen90Tests/TripDateExpanderTests.swift
@@ -5,9 +5,44 @@
 //  Created by Som Kundu on 26/07/26.
 //
 
+import Foundation
 import Testing
 @testable import Schengen90
 
 struct TripDateExpanderTests {
 
+    @Test
+    func singleTripProducesCorrectDates() {
+
+        let trip = Trip(
+            entryDate: DateFormatter.tripDate.date(from: "10 Jan 2026")!,
+            exitDate: DateFormatter.tripDate.date(from: "12 Jan 2026")!
+        )
+
+        let expander = TripDateExpander()
+
+        let dates = expander.occupiedDates(from: [trip])
+
+        #expect(dates.count == 3)
+    }
+
+    @Test
+    func sameDayReentryCountsDayOnlyOnce() {
+
+        let trip1 = Trip(
+            entryDate: DateFormatter.tripDate.date(from: "10 Jan 2026")!,
+            exitDate: DateFormatter.tripDate.date(from: "15 Jan 2026")!
+        )
+
+        let trip2 = Trip(
+            entryDate: DateFormatter.tripDate.date(from: "15 Jan 2026")!,
+            exitDate: DateFormatter.tripDate.date(from: "21 Jan 2026")!
+        )
+
+        let expander = TripDateExpander()
+
+        let dates = expander.occupiedDates(from: [trip1, trip2])
+
+        #expect(dates.count == 12)
+    }
 }
