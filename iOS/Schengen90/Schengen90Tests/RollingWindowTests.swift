428a3158ba310d02d74b29b42c521fa7da3dbfa8
som-db2 <sskundu2025@gmail.com>
2026-07-26 22:49:50 +0530

    Update RollingWindowTests.swift

--- iOS/Schengen90/Schengen90Tests/RollingWindowTests.swift
+++ iOS/Schengen90/Schengen90Tests/RollingWindowTests.swift
@@ -11,4 +11,30 @@ import Testing
 
 struct RollingWindowTests {
 
+    @Test
+    func filtersDatesInsideWindow() {
+
+        let formatter = DateFormatter.tripDate
+
+        let dates: Set<Date> = [
+
+            formatter.date(from: "01 Jan 2026")!,
+            formatter.date(from: "15 Feb 2026")!,
+            formatter.date(from: "10 Jul 2026")!
+
+        ]
+
+        let reference = formatter.date(from: "26 Jul 2026")!
+
+        let rollingWindow = RollingWindow()
+
+        let result = rollingWindow.dates(
+            from: dates,
+            referenceDate: reference
+        )
+
+        #expect(result.count == 2)
+
+    }
+
 }
