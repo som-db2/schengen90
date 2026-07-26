7ab1e4a3b58b9d5a1282ef52fc43e53438565699
som-db2 <sskundu2025@gmail.com>
2026-07-26 16:11:07 +0530

    Update TripValidator.swift

--- iOS/Schengen90/Schengen90/Services/TripValidator.swift
+++ iOS/Schengen90/Schengen90/Services/TripValidator.swift
@@ -5,3 +5,27 @@
 //  Created by Som Kundu on 26/07/26.
 //
 
+import Foundation
+
+struct TripValidator {
+
+    static func validate(
+        entryDate: Date,
+        exitDate: Date
+    ) -> [String] {
+
+        var errors: [String] = []
+
+        if exitDate < entryDate {
+
+            errors.append(
+                "Exit date must be on or after the entry date."
+            )
+
+        }
+
+        return errors
+
+    }
+
+}
