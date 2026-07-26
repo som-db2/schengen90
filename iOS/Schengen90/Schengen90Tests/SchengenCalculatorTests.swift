cfddaeeaf3ecafb882502e3d328a4a83d24467c3
som-db2 <sskundu2025@gmail.com>
2026-07-26 22:59:39 +0530

    Update SchengenCalculatorTests.swift

--- iOS/Schengen90/Schengen90Tests/SchengenCalculatorTests.swift
+++ iOS/Schengen90/Schengen90Tests/SchengenCalculatorTests.swift
@@ -5,9 +5,31 @@
 //  Created by Som Kundu on 26/07/26.
 //
 
+import Foundation
 import Testing
 @testable import Schengen90
 
 struct SchengenCalculatorTests {
+
+    @Test
+    func usedDaysForSingleTrip() {
+
+        let formatter = DateFormatter.tripDate
+
+        let trip = Trip(
+            entryDate: formatter.date(from: "10 Jul 2026")!,
+            exitDate: formatter.date(from: "15 Jul 2026")!
+        )
+
+        let calculator = SchengenCalculator()
+
+        let used = calculator.usedDays(
+            from: [trip],
+            on: formatter.date(from: "26 Jul 2026")!
+        )
+
+        #expect(used == 6)
+
+    }
 
 }
