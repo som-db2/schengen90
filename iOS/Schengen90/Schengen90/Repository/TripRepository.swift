cbec0709c0879f6adcdd8b8111aef483488c40b9
som-db2 <sskundu2025@gmail.com>
2026-07-27 23:08:00 +0530

    Create TripRepository.swift

---
+++ iOS/Schengen90/Schengen90/Repository/TripRepository.swift
@@ -0,0 +1,22 @@
+//
+//  TripRepository.swift
+//  Schengen90
+//
+//  Created by Som Kundu on 27/07/26.
+//
+
+import Foundation
+import SwiftData
+
+@Observable
+final class TripRepository {
+
+    private let modelContext: ModelContext
+
+    init(modelContext: ModelContext) {
+
+        self.modelContext = modelContext
+
+    }
+
+}
