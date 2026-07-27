473a9c1656a7a3598c8f0ee7894e3169552bbf16
som-db2 <sskundu2025@gmail.com>
2026-07-27 17:28:31 +0530

    Create PlannerServiceTests.swift

---
+++ iOS/Schengen90/Schengen90Tests/PlannerServiceTests.swift
@@ -0,0 +1,22 @@
+//
+//  PlannerServiceTests.swift
+//  Schengen90
+//
+//  Created by Som Kundu on 27/07/26.
+//
+
+import Testing
+import Foundation
+
+@testable import Schengen90
+
+struct PlannerServiceTests {
+
+    @Test
+    func plannerServiceCanBeCreated() {
+
+        _ = PlannerService()
+
+    }
+
+}
