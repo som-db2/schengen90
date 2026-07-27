5897c73bc6a2b9824ece105a3f05fb815f554511
som-db2 <sskundu2025@gmail.com>
2026-07-28 00:45:56 +0530

    Update PlannerService.swift

--- iOS/Schengen90/Schengen90/Services/PlannerService.swift
+++ iOS/Schengen90/Schengen90/Services/PlannerService.swift
@@ -5,11 +5,36 @@
 //  Created by Som Kundu on 27/07/26.
 //
 
+//import Foundation
+
+//struct PlannerService {
+
+//    private let calculator = SchengenCalculator()
+
+//    func planTrip(
+  //      existingTrips: [Trip],
+    //    proposedEntryDate: Date
+  //  ) -> StaySimulationResult? {
+
+    //    let occupiedDates = TripDateExpander()
+      //      .occupiedDates(from: existingTrips)
+
+      //  return StaySimulator().simulate(
+        //    occupiedDates: occupiedDates,
+          //  proposedEntryDate: proposedEntryDate
+       // )
+
+//    }
+
+//}
+
+//
+//  PlannerService.swift
+//
+
 import Foundation
 
 struct PlannerService {
-
-    private let calculator = SchengenCalculator()
 
     func planTrip(
         existingTrips: [Trip],
@@ -18,6 +43,13 @@ struct PlannerService {
 
         let occupiedDates = TripDateExpander()
             .occupiedDates(from: existingTrips)
+
+        print("===================================")
+        print("Planner Debug")
+        print("Historical Trips : \(existingTrips.count)")
+        print("Occupied Days    : \(occupiedDates.count)")
+        print("Proposed Entry   : \(proposedEntryDate)")
+        print("===================================")
 
         return StaySimulator().simulate(
             occupiedDates: occupiedDates,
